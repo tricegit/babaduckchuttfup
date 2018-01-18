15e87f7c274ab1b581d61019299592b6b0a9792e
Robin Lundin <Robin@Robins-MacBook-Pro.local>
2018-01-18 20:19:32 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import MainChat from './main-chat';
+
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <MainChat/>
+    );
+  }
+}
+
+export default App;
---
+++ src/main-chat/index.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class MainChat extends Component {
+
+    render() {
+        return <div>Chat!</div>
+    }
+}
+
+export default MainChat;
